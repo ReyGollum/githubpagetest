91fe3e5e627252874639be3428bbb7f48d2339b2
ReyGollum <reyshoons@gmail.com>
2024-11-18 17:26:42 -0600

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,24 @@
+/***
+Autor: Rey Manuel Rodriguez Aguilar
+Fecha: 16/10/2024
+ ***/
+
+for (var i = 1, j = 1; i <= 100; i++){
+
+    console.log(i);
+
+    if (i == 7 * j){
+        console.log(i, "Multiplo de 7");
+
+            j++;
+        }
+        else{
+            console.log(i,"No es multiplo de 7");
+        }
+    }
+
+
+
+
+
+
